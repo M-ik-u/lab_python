132c2ce7d68491ab6efa90bea64b6b6d7adc2111
M-ik-u <bsuprogrammist@gmail.com>
2025-09-29 21:49:20 +0300

    task8

---
+++ task_10.py
@@ -0,0 +1,9 @@
+a = int(input("enter a: "))
+b = int(input("enter b: "))
+
+print(f"Сумма = {a + b}"
+      f"\nРазность = { a - b}"
+      f"\nПроизведение = { a * b}"
+      f"\nЧастное от дел = {float(a / b)}"
+      f"\nОстаток от дел = {a % b}"
+      f"\nСтепень a^b = {pow(a,b)}")
