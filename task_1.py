deda167c19f622190dc9ca6110a3573430c249a4
M-ik-u <bsuprogrammist@gmail.com>
2025-09-29 14:39:42 +0300

    task1

---
+++ task_1.py
@@ -0,0 +1,5 @@
+surname = input("Введите фамилию ")
+name = input("Введите имя ")
+middle_name = input("Введите отчество ")
+
+print(f"{surname.capitalize()} {name[0].upper()}.{middle_name[0].upper()}.")
